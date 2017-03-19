6c3a43bb63305586e5506f23b8cc10da65853438
GrimmOutlook <grimmoutlook87@gmail.com>
2017-03-19 14:00:51 -0400

    Added quiz questions and answers to quiz.js.

---
+++ js/quiz.js
@@ -0,0 +1,46 @@
+var quiz = [
+  {
+    question1: "How many Super Bowls have the Steelers won?",
+    answer1: 6
+  },
+  {
+    question2: "How many Super Bowls have the Steelers played in?",
+    answer2: 8
+  },
+  {
+    question3: "Who was Super Bowl MVP in Super Bowl IX?",
+    answer3: "Franco Harris"
+  },
+  {
+    question4: "How many times have the Steelers played the Dallas Cowboys in the Super Bowl?",
+    answer4: 3
+  },
+  {
+    question5: "What Steeler has the longest TD run in Super Bowl history?",
+    answer5: "Willie Parker"
+  },
+  {
+    question6: "Who did the Steelers defeat in Super Bowl XIV?",
+    answer6: "Los Angeles Rams"
+  },
+  {
+    question7: "How many TD passes did Ben Roethlisberger throw in Super Bowl XL?",
+    answer7: 0
+  },
+  {
+    question8: "How many TD passes did WR Antwaan Randle-El throw in Super Bowl XL?",
+    answer8: 1
+  },
+  {
+    question9: "Who did the Steelers defeat in Super Bowl X?",
+    answer9: "Dallas Cowboys"
+  },
+  {
+    question10: "What Steeler has the longest Interception TD return in Super Bowl history?",
+    answer10: "James Harrison"
+  },
+
+
+
+
+]
