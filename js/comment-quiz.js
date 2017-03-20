60bfd23a92895fcade154bcb01c6ae1a2508d317
GrimmOutlook <grimmoutlook87@gmail.com>
2017-03-20 13:44:18 -0400

    Added multiple choice answers and app rough outline in comment-quiz.js

---
+++ js/comment-quiz.js
@@ -0,0 +1,17 @@
+// An array of objects of questions, answers, & 3 incorrect answers is assigned to var quiz.
+
+// An object, var guesses, is created with one key, userGuess, and an empty array for a value.
+
+// An empty array, correctGuesses, is created.
+
+// A fxn that takes the userGuess inputs and pushes them to the array value in guesses is created.  Modifies the state of the guess object.
+
+// A fxn that takes each of the userGuess array items & compares them to the correct answers in var quiz.  If equal, mark as correct, if not, mark as incorrect.
+
+// A fxn that has a counter to keep track of what question is being asked.
+
+// A fxn that displays the quiz question & multiple choice guesses.  Once guess is made, the next question & multiple choices are displayed.
+
+// A fxn that displays the question number counter.
+
+// A fxn that displays the correct questions and incorrect questions.
--- js/quiz.js
+++ js/quiz.js
@@ -2,45 +2,75 @@ var quiz = [
   {
     question1: "How many Super Bowls have the Steelers won?",
     answer1: 6
+    choice1_1: 4
+    choice1_2: 5
+    choice1_3: 1
   },
   {
     question2: "How many Super Bowls have the Steelers played in?",
     answer2: 8
+    choice2_1: 4
+    choice2_2: 5
+    choice2_3: 10
   },
   {
     question3: "Who was Super Bowl MVP in Super Bowl IX?",
     answer3: "Franco Harris"
+    choice3_1: "Terry Bradshaw"
+    choice3_2: "Dwight White"
+    choice3_3: "Lynn Swann"
   },
   {
     question4: "How many times have the Steelers played the Dallas Cowboys in the Super Bowl?",
     answer4: 3
+    choice4_1: 0
+    choice4_2: 2
+    choice4_3: 1
   },
   {
     question5: "What Steeler has the longest TD run in Super Bowl history?",
     answer5: "Willie Parker"
+    choice5_1: "Jerome Bettis"
+    choice5_2: "Franco Harris"
+    choice5_3: "Walter Abercrombie"
   },
   {
     question6: "Who did the Steelers defeat in Super Bowl XIV?",
     answer6: "Los Angeles Rams"
+    choice6_1: "Dallas Cowboys"
+    choice6_2: "San Francisco 49ers"
+    choice6_3: "Detroit Lions"
   },
   {
     question7: "How many TD passes did Ben Roethlisberger throw in Super Bowl XL?",
     answer7: 0
+    choice7_1: 5
+    choice7_2: 2
+    choice7_3: 1
   },
   {
     question8: "How many TD passes did WR Antwaan Randle-El throw in Super Bowl XL?",
     answer8: 1
+    choice8_1: 3
+    choice8_2: 0
+    choice8_3: 2
   },
   {
     question9: "Who did the Steelers defeat in Super Bowl X?",
     answer9: "Dallas Cowboys"
+    choice6_1: "Minnesota Vikings"
+    choice6_2: "San Francisco 49ers"
+    choice6_3: "New York Giants"
   },
   {
     question10: "What Steeler has the longest Interception TD return in Super Bowl history?",
     answer10: "James Harrison"
+    choice6_1: "Mel Blount"
+    choice6_2: "Carnell Lake"
+    choice6_3: "Troy Polamalu"
   },
+];
 
+var userAnswer;
 
-
-
-]
+console.log(quiz[9].question10);
