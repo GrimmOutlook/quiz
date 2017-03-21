a8a0f44bd9bbd288211b54677aa56a7aac1bd81e
GrimmOutlook <grimmoutlook87@gmail.com>
2017-03-20 21:11:25 -0400

    Coded var state and several fxns, compressed quiz objects.

--- js/comment-quiz.js
+++ js/comment-quiz.js
@@ -1,16 +1,52 @@
+import quiz from "./quiz";
+
 // An array of objects of questions, answers, & 3 incorrect answers is assigned to var quiz.
 
-// An object, var guesses, is created with one key, userGuess, and an empty array for a value.
+// An object, var guesses, is created with one key, userGuesses, and an empty array for a value.
 
 // An empty array, correctGuesses, is created.
 
+var state = {
+  userGuess: "",
+  correctGuesses: 0,
+  incorrectGuesses: 0,
+  currentQuestion: 1,
+  quizQuestion: {}
+};
+
+var totalQuestions = 5;
+
 // A fxn that takes the userGuess inputs and pushes them to the array value in guesses is created.  Modifies the state of the guess object.
+var questionCounter = function(state){
+  state.currentQuestion += 1;
+}
+
+var correctCounter = function(state){
+  state.correctGuesses += 1;
+}
+
+var incorrectCounter = function(state){
+  state.incorrectGuesses += 1;
+}
+
+var userGuessTracker = function(state, guess){
+  state.userGuess = guess;
+}
+
+var questionNow = function(state, quiz){
+  state.quizQuestion = quiz[Math.random(something to pick a question)];
+}
+
+var guessComparison = function(state, quiz){
+  quiz[0].answer === state.userGuess;
+}
+
 
 // A fxn that takes each of the userGuess array items & compares them to the correct answers in var quiz.  If equal, mark as correct, if not, mark as incorrect.
 
 // A fxn that has a counter to keep track of what question is being asked.
 
-// A fxn that displays the quiz question & multiple choice guesses.  Once guess is made, the next question & multiple choices are displayed.
+// A fxn that randomly displays the quiz question, along with displaying its 4 multiple choice guesses in a random order.  Once guess is made, the next question & multiple choices are displayed.
 
 // A fxn that displays the question number counter.
 
--- js/quiz.js
+++ js/quiz.js
@@ -1,76 +1,56 @@
 var quiz = [
   {
-    question1: "How many Super Bowls have the Steelers won?",
-    answer1: 6
-    choice1_1: 4
-    choice1_2: 5
-    choice1_3: 1
+    question: "How many Super Bowls have the Steelers won?",
+    answer: 6,
+    choices: [4,5,1]
   },
   {
-    question2: "How many Super Bowls have the Steelers played in?",
-    answer2: 8
-    choice2_1: 4
-    choice2_2: 5
-    choice2_3: 10
+    question: "How many Super Bowls have the Steelers played in?",
+    answer: 8,
+    choices: [4,5,10]
   },
   {
-    question3: "Who was Super Bowl MVP in Super Bowl IX?",
-    answer3: "Franco Harris"
-    choice3_1: "Terry Bradshaw"
-    choice3_2: "Dwight White"
-    choice3_3: "Lynn Swann"
+    question: "Who was Super Bowl MVP in Super Bowl IX?",
+    answer: "Franco Harris",
+    choices: ["Terry Bradshaw", "Dwight White", "Lynn Swann"]
   },
   {
-    question4: "How many times have the Steelers played the Dallas Cowboys in the Super Bowl?",
-    answer4: 3
-    choice4_1: 0
-    choice4_2: 2
-    choice4_3: 1
+    question: "How many times have the Steelers played the Dallas Cowboys in the Super Bowl?",
+    answer: 3,
+    choices: [0,2,1]
   },
   {
-    question5: "What Steeler has the longest TD run in Super Bowl history?",
-    answer5: "Willie Parker"
-    choice5_1: "Jerome Bettis"
-    choice5_2: "Franco Harris"
-    choice5_3: "Walter Abercrombie"
+    question: "What Steeler has the longest TD run in Super Bowl history?",
+    answer: "Willie Parker",
+    choices: ["Jerome Bettis", "Franco Harris", "Walter Abercrombie"]
   },
   {
-    question6: "Who did the Steelers defeat in Super Bowl XIV?",
-    answer6: "Los Angeles Rams"
-    choice6_1: "Dallas Cowboys"
-    choice6_2: "San Francisco 49ers"
-    choice6_3: "Detroit Lions"
+    question: "Who did the Steelers defeat in Super Bowl XIV?",
+    answer: "Los Angeles Rams",
+    choices: ["Dallas Cowboys", "San Francisco 49ers", "Detroit Lions"]
   },
   {
-    question7: "How many TD passes did Ben Roethlisberger throw in Super Bowl XL?",
-    answer7: 0
-    choice7_1: 5
-    choice7_2: 2
-    choice7_3: 1
+    question: "How many TD passes did Ben Roethlisberger throw in Super Bowl XL?",
+    answer: 0,
+    choices: [5,2,1]
   },
   {
-    question8: "How many TD passes did WR Antwaan Randle-El throw in Super Bowl XL?",
-    answer8: 1
-    choice8_1: 3
-    choice8_2: 0
-    choice8_3: 2
+    question: "How many TD passes did WR Antwaan Randle-El throw in Super Bowl XL?",
+    answer: 1,
+    choices: [3,0,2]
   },
   {
-    question9: "Who did the Steelers defeat in Super Bowl X?",
-    answer9: "Dallas Cowboys"
-    choice6_1: "Minnesota Vikings"
-    choice6_2: "San Francisco 49ers"
-    choice6_3: "New York Giants"
+    question: "Who did the Steelers defeat in Super Bowl X?",
+    answer: "Dallas Cowboys",
+    choices: ["Minnesota Vikings", "San Francisco 49ers", "New York Giants"]
   },
   {
-    question10: "What Steeler has the longest Interception TD return in Super Bowl history?",
-    answer10: "James Harrison"
-    choice6_1: "Mel Blount"
-    choice6_2: "Carnell Lake"
-    choice6_3: "Troy Polamalu"
+    question: "What Steeler has the longest Interception TD return in Super Bowl history?",
+    answer: "James Harrison",
+    choices: ["Mel Blount", "Carnell Lake", "Troy Polamalu"]
   },
 ];
 
-var userAnswer;
+console.log(quiz[9].choice10_3);
 
-console.log(quiz[9].question10);
+export default quiz;
